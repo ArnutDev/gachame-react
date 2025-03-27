5450132c0aca0e66e9116a5cc29cc87902544b0e
ArnutDev <arnutbuadonpai@gmail.com>
2025-03-27 22:28:34 +0700

    Create Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 function Footer() {
     return (
-        <nav className="Footer">
-            <div className="logo">React Router</div>
-            <ul className="nav-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/update">Update</a></li>
-            </ul>
-        </nav>
+        <footer className="bg-warning text-dark mt-0 py-3">
+            <div className="container text-center">
+                <p className="mb-0">GachaMe is a LINE Rangers Gacha simulation website
+                    that is made for entertainment purposes only. This website uses
+                    some information from the <a href="https://rangers.lerico.net/en/rangers-book" target="_blank">LINE
+                        Rangers Handbook</a> and the LINE Rangers game.
+                    This website is not an official website for the LINE Rangers game.
+                    No personal user data is collected, but each gacha draw is collected
+                    to generate statistical data.</p>
+            </div>
+        </footer>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 function Home() {
     return (
-        <div>
-            <h1>Welcome to the Home Page!</h1>
-            <p>This is the main page of our website.</p>
+        <div className="d-flex flex-column min-vh-100">
+            <div className="flex-grow-1">
+                <h1>Welcome to the Home Page!</h1>
+                <p>This is the main page of our website.</p>
+            </div>
+            <Footer />
         </div>
+
+
     );
 }
 
