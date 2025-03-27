852f33c36efe62c92fd16210153f88109807347d
ArnutDev <arnutbuadonpai@gmail.com>
2025-03-27 22:28:47 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,13 +12,13 @@ function Navbar() {
                 <div id="navbar1" class="collapse navbar-collapse">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item mx-2">
-                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/" className="nav-link text-light">Home</Link></li>
                         </li>
                         <li class="nav-item mx-2">
-                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/about" className="nav-link text-light">About</Link></li>
                         </li>
                         <li class="nav-item mx-2">
-                            <li><Link to="/update">Update</Link></li>
+                            <li><Link to="/update" className="nav-link text-light">Update</Link></li>
                         </li>
                     </ul>
                 </div>
