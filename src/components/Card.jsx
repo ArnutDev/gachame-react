129b190772cd7c42b9d9dd030151465a625d4a3c
ArnutDev <arnutbuadonpai@gmail.com>
2025-03-28 23:47:50 +0700

    Update Card.jsx
    use object for keep data that different

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-function Card({ value }) {
+function Card({ cardDetails }) {
 
     return (
         <a href="rangers-rand.html" className="text-decoration-none">
             <div className="card">
-                <img src={"assets/" + value} className="card-img-top" alt="Card Image" />
+                <img src={"assets/" + cardDetails.image} className="card-img-top" alt="Card Image" />
                 <div className="card-body">
-                    <h5 className="card-title text-dark">Gacha Rangers 6+1 Click!</h5>
+                    <h5 className="card-title text-dark">{cardDetails.topic}</h5>
                     <p className="card-text mb-3 text-dark">
-                        The new rangers are waiting for you!!!
+                        {cardDetails.detail}
                     </p>
                     <div className="btn-container d-flex justify-content-end">
                         <a href="rangers-rand.html" className="btn btn-primary">Click</a>
