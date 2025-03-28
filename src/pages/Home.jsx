2144ef229ca48420bc447f982f331fc7c406d57f
ArnutDev <arnutbuadonpai@gmail.com>
2025-03-28 23:28:01 +0700

    Update Home.jsx
    use head and can set any value when use again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Footer from '../components/Footer';
-
+import Head from '../components/Head';
+import Card from '../components/Card';
 function Home() {
     return (
         <div className="d-flex flex-column min-vh-100">
-            <div className="flex-grow-1">
-                <h1>Welcome to the Home Page!</h1>
-                <p>This is the main page of our website.</p>
+            <div className="flex-fill">
+                <Head value=" Pick your gacha type " />
+                <div className="container mt-4">
+                    <div className="row">
+                        <div className="col-md-6 mb-5">
+                            <Card />
+                        </div>
+                        <div className="col-md-6 mb-5">
+                            <Card />
+                        </div>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
